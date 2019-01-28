740270ddf37f9599da21b1ced21a57367f0973c1
Mukul <mukulbisht98@gmail.com>
2019-01-28 13:28:31 +0530

    added splash screen implementation

--- app/src/main/java/com/example/geddi/MainActivity.java
+++ app/src/main/java/com/example/geddi/MainActivity.java
@@ -1,29 +1,26 @@
 package com.example.geddi;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
-import android.widget.ImageButton;
-
-import java.util.Vector;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    EditText searh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        searh = (EditText) findViewById(R.id.search);
-        Button next = (Button) findViewById(R.id.next);
-        next.setOnClickListener(new View.OnClickListener() {
+
+
+        int STATIC_SPLASH_TIMEOUT = 3000;
+        new Handler().postDelayed(new Runnable() {
             @Override
-            public void onClick(View v) {
-                String str= searh.getText().toString().trim();
-                searh.setText("knkj");
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
             }
-        });
+        }, STATIC_SPLASH_TIMEOUT);
     }
 }
