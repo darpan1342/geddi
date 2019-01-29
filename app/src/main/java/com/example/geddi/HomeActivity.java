fb9e56175c4b31884bd12406387d2f6c1ceeb1af
darpan1342 <asharma6890@gmail.com>
2019-01-29 14:39:08 +0530

    *

--- app/src/main/java/com/example/geddi/HomeActivity.java
+++ app/src/main/java/com/example/geddi/HomeActivity.java
@@ -12,7 +12,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_home);
         ViewPager fragment= (ViewPager)findViewById(R.id.viewpager);
         FragmentPager adapter= new FragmentPager(getSupportFragmentManager());
         fragment.setAdapter(adapter);
