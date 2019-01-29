cf67251a9a127629c8b82c1080bc99e855f0fc56
darpan1342 <asharma6890@gmail.com>
2019-01-29 15:30:51 +0530

    fragmentation and splash screen

---
+++ app/src/main/java/com/example/geddi/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.geddi;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        int STATIC_SPLASH_TIMEOUT = 3000;
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent homeIntent = new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(homeIntent);
+                finish();
+            }
+        }, STATIC_SPLASH_TIMEOUT);
+    }
+}
