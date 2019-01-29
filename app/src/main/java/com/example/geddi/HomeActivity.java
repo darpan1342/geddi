8c21c5488371b95da12ffa99adcc0cb1c820684e
darpan1342 <asharma6890@gmail.com>
2019-01-29 19:03:21 +0530

    .

--- app/src/main/java/com/example/geddi/HomeActivity.java
+++ app/src/main/java/com/example/geddi/HomeActivity.java
@@ -1,13 +1,30 @@
 package com.example.geddi;
 
 import android.os.Bundle;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class HomeActivity extends AppCompatActivity {
+    EditText searh;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        ViewPager fragment= (ViewPager)findViewById(R.id.viewpager);
+        FragmentPager adapter= new FragmentPager(getSupportFragmentManager());
+        fragment.setAdapter(adapter);
+
+        /*searh = (EditText) findViewById(R.id.search);
+        Button next = (Button) findViewById(R.id.next);
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String str= searh.getText().toString().trim();
+                searh.setText("knkj");
+            }
+        });*/
     }
 }
