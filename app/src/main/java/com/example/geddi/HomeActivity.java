20edc4a1bf8131a60c0921bffd2e2efcf6470d74
Mukul <mukulbisht98@gmail.com>
2019-02-02 16:37:31 +0530

    changed a bit in navigation button

--- app/src/main/java/com/example/geddi/HomeActivity.java
+++ app/src/main/java/com/example/geddi/HomeActivity.java
@@ -4,8 +4,8 @@ import android.os.Bundle;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity {
     EditText searh;
@@ -15,17 +15,17 @@ public class HomeActivity extends AppCompatActivity {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        final ViewPager fragment= (ViewPager)findViewById(R.id.viewpager);
-        FragmentPager adapter= new FragmentPager(getSupportFragmentManager());
+        final ViewPager fragment = findViewById(R.id.viewpager);
+        FragmentPager adapter = new FragmentPager(getSupportFragmentManager());
         fragment.setAdapter(adapter);
-        Button search = (Button) findViewById(R.id.search_page);
-        Button add = (Button) findViewById(R.id.add);
-        Button account = (Button) findViewById(R.id.account);
-        Button home = (Button) findViewById(R.id.home);
+        ImageButton search = findViewById(R.id.search_page);
+        ImageButton add = findViewById(R.id.add);
+        ImageButton account = findViewById(R.id.account);
+        ImageButton home = findViewById(R.id.home);
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-            fragment.setCurrentItem(1);
+                fragment.setCurrentItem(1);
             }
         });
         home.setOnClickListener(new View.OnClickListener() {
