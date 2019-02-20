8e0f5a3af1a1bbe52b91c6e48dbdf67ad7546fde
BurnedQuaterBack <36494771+darpan1342@users.noreply.github.com>
2019-02-20 21:34:58 +0530

    Update HomeActivity.java

--- app/src/main/java/com/example/geddi/HomeActivity.java
+++ app/src/main/java/com/example/geddi/HomeActivity.java
@@ -1,26 +1,50 @@
 package com.example.geddi;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.EditText;
-import android.widget.ImageButton;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class HomeActivity extends AppCompatActivity {
+    double latitude;
+    double longitude;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
 
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
-        final ViewPager fragment = findViewById(R.id.viewpager);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            requestPermissions(new String[]{
+                    Manifest.permission.ACCESS_FINE_LOCATION,Manifest.permission.ACCESS_COARSE_LOCATION,Manifest.permission.INTERNET
+                    ,Manifest.permission.ACCESS_NETWORK_STATE,Manifest.permission.READ_PHONE_STATE
+            },10);
+        }
+        else
+        {
+            GpsLocation gpsLocation = new GpsLocation(this);
+            latitude = gpsLocation.getLatitude();
+            longitude = gpsLocation.getLongitude();
+            TextView t = (TextView) findViewById(R.id.asd);
+            t.setText("/n" + latitude + "  " + longitude);
+        }
+        final ViewPager fragment = (ViewPager) findViewById(R.id.viewpager);
         FragmentPager adapter = new FragmentPager(getSupportFragmentManager());
         fragment.setAdapter(adapter);
-        ImageButton search = findViewById(R.id.search_page);
-        ImageButton add = findViewById(R.id.add);
-        ImageButton account = findViewById(R.id.account);
-        ImageButton home = findViewById(R.id.home);
+        Button search = (Button) findViewById(R.id.search_page);
+        Button add = (Button) findViewById(R.id.add);
+        Button account = (Button) findViewById(R.id.account);
+        Button home = (Button) findViewById(R.id.home);
         search.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -45,5 +69,36 @@ public class HomeActivity extends AppCompatActivity {
                 fragment.setCurrentItem(3);
             }
         });
+
+        /*Geocoder gcd = new Geocoder(this, Locale.getDefault());
+        try {
+            List<Address> addressList = gcd.getFromLocation(latitude,longitude,1);
+            if(addressList.size()>0)
+            {
+                t.setText(addressList.get(0).getSubAdminArea());
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }*/
+
     }
+
+    public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                                              int[] grantResults){
+        switch (requestCode)
+        {
+            case 10: {
+                GpsLocation gpsLocation = new GpsLocation(this);
+                latitude = gpsLocation.getLatitude();
+                longitude = gpsLocation.getLongitude();
+                TextView t = (TextView) findViewById(R.id.asd);
+                t.setText("/n" + latitude + "  " + longitude);
+            }
+                break;
+                default:
+                    break;
+        }
+    }
+
+
 }
